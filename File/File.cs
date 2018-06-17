4061ab7fce173432d97742337fa764954d44f806
Unknown <manuilowa97@gmail.com>
2018-06-17 14:37:54 +0300

    Initial commit

---
+++ File/File.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace File
+{
+    class File
+    {
+        string lineText, tmp, path;
+        int count = 0;
+
+        public string Input()
+        {
+            tmp = Console.ReadLine();
+            return tmp;
+        }
+
+        public void findWord(string word, string p)
+        {
+            Regex rgx = new Regex(word);
+            lineText = readFile(p);
+
+            foreach (Match match in rgx.Matches(lineText))
+            {
+                count++;
+                
+            }
+
+            Console.WriteLine("В файле найдено {0} раз слово {1} ", count, word);
+        }
+
+        public void replaceWord(string p, string oldWord, string newWord)
+        {
+            lineText = readFile(p);
+            Regex rgx = new Regex(oldWord);
+            string result = rgx.Replace(lineText, newWord);
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(path);
+                writer.Write(result);
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Replaced!");
+
+        }
+
+        private string readFile(string p)
+        {
+            try
+            {
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, p);
+                StreamReader reader = new StreamReader(path);
+                lineText = reader.ReadToEnd();
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return lineText;
+        }
+
+    }
+}
---
+++ File/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File f = new File();
+            string path, word, word2, task;
+
+            Console.WriteLine("Enter the path to file:");
+            path = f.Input();
+
+            Console.WriteLine("Enter the number of task:" +
+                "\n Find the number of occurrences of a string in a text file  => 1" +
+                "\n To replace a string with another in the specified file => 2");
+            task = Console.ReadLine();
+
+            switch (task)
+            {
+                case "1":
+                    Console.WriteLine("Enter the word:");
+                    word = f.Input();
+                    f.findWord(word, path);
+                    break;
+                case "2":
+                    Console.WriteLine("Enter the word:");
+                    word = f.Input();
+                    Console.WriteLine("Enter the word for replace:");
+                    word2 = f.Input();
+                    f.replaceWord(path, word, word2);
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
